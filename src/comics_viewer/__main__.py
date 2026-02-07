801d62e8154d65543fe1cf8cb2c1e1284d869cdb
tkonya <thomas.konya@gmail.com>
2026-02-07 17:05:42 +0100

    added argparse

--- src/comics_viewer/__main__.py
+++ src/comics_viewer/__main__.py
@@ -3,6 +3,7 @@ import numpy as np
 from functools import partial
 from contextlib import ExitStack
 import signal
+import argparse
 
 from .library import Library
 from .view import View
@@ -12,12 +13,19 @@ from .gi_helpers import GLib
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cache", default=Path("./data/cache"))
+    parser.add_argument("--library", default=Path("~/tmp/foo").expanduser())
+    parser.add_argument("--database",
+                         default=Path("./data/db.sqlite").absolute())
+    args = parser.parse_args()
+
     with ExitStack() as stack:
-        TEMP = Path("./data/cache").absolute()
+        TEMP = args.cache
         create_library = partial(
             Library,
-            library=Path("~/tmp/foo").expanduser().absolute(),
-            db=Path("./data/db.sqlite").absolute(),
+            library=args.library.expanduser().absolute(),
+            db=Path(args.database).absolute(),
             cover_cache=stack.enter_context(
                 CoverCache(TEMP / "cover", np.array([240, 240]))
             ),
@@ -26,7 +34,7 @@ def main():
             CoverCache(TEMP / "thumb", np.array([120, 120]))
         ))
         app = App(stack, create_library, create_view,
-                  application_id="com.gitlab.some00.comics_viewer",)
+                  application_id="com.github.some00.comics_viewer",)
         for sig in [signal.SIGINT, signal.SIGTERM]:
             GLib.unix_signal_add(GLib.PRIORITY_DEFAULT, sig,
                                  lambda *x: app.quit(), None)
