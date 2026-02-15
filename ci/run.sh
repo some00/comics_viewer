fa7a50950484b277563c9159175c86ea2ffcb898
Konya Tamas <thomas.konya@gmail.com>
2026-02-15 17:27:47 +0100

    Create venv in tmp

--- ci/run.sh
+++ ci/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
-uv venv .venv_docker
-source .venv_docker/bin/activate
+uv venv /tmp/.venv_docker
+source /tmp/.venv_docker/bin/activate
 uv pip install '.[test]'
 pytest --cov=comics_viewer
 pyrefly check
