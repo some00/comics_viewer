a64ddcbf40865a3408d1fdf4defab32ef80f6269
tkonya <thomas.konya@gmail.com>
2026-02-16 21:21:51 +0100

    test_thumb finished

--- test/test_comics_viewer/test_thumb.py
+++ test/test_comics_viewer/test_thumb.py
@@ -1,12 +1,16 @@
-from pathlib import Path
 import pytest
 from typing import Iterable
 from pathlib import Path
+from PIL import Image as image
 
 from comics_viewer.thumb import PageInfo, Thumb
 from comics_viewer.gi_helpers import GObject, Gtk, GLib
 from comics_viewer.view import View
 from comics_viewer.cover_cache import CoverCache
+from comics_viewer.archive import Archive
+
+
+BASE_DIR = Path(__file__).parent.parent / "data"
 
 
 def test_page_info():
@@ -43,11 +47,8 @@ def mock_builder(mocker) -> Gtk.Builder:
 
 @pytest.fixture
 def mock_glib_idle_source(mocker) -> Iterable[GLib.Source]:
-    with mocker.patch(
-        "comics_viewer.thumb.GLib.idle_source_new",
-        spec=GLib.Source,
-    ) as source:
-        yield source
+    return mocker.patch("comics_viewer.thumb.GLib.idle_source_new",
+                        spec=GLib.Source)
 
 
 @pytest.fixture
@@ -81,9 +82,117 @@ def thumb(mock_view, mock_builder, mock_cover_cache, mock_flowbox,
     thumb = Thumb(view=mock_view,
                   builder=mock_builder,
                   thumb_cache=mock_cover_cache,
-                  library=Path("library"))
+                  library=BASE_DIR)
     return thumb
 
 
-def test_archive(thumb):
+def test_archive_none(thumb):
     assert thumb.archive is None
+
+
+@pytest.fixture
+def mock_builder_in_source(mocker) -> Gtk.Builder:
+    return mocker.patch("comics_viewer.thumb.Gtk.Builder",
+                        spec=Gtk.Builder)
+
+
+@pytest.fixture
+def mock_box(mocker):
+    return mocker.MagicMock(spec=Gtk.Box)
+
+
+@pytest.fixture
+def mock_img(mocker):
+    return mocker.MagicMock(spec=Gtk.Image)
+
+
+@pytest.fixture
+def mock_label(mocker):
+    return mocker.MagicMock(spec=Gtk.Label)
+
+
+def test_create_thumb(
+    thumb, mock_builder_in_source, mock_glib_idle_source, mock_box, mock_img,
+    mock_label,
+):
+    builder = mock_builder_in_source.return_value
+    builder.get_object.side_effect = [mock_img, mock_label, mock_box]
+    box = thumb.create_thumb(PageInfo(path=Path("a.cbz"), page_idx=5))
+    assert box == mock_box
+    assert box.page_idx == 5
+    builder.add_from_file.assert_called()
+    mock_label.set_label.assert_called_once_with("6")
+    mock_glib_idle_source.assert_called_once()
+
+
+@pytest.fixture
+def thumb_create_thumb_called(
+    thumb, mock_builder_in_source, mock_glib_idle_source, mock_box, mock_img,
+    mock_label,
+):
+    mock_glib_idle_source  # silence unused
+    thumb.archive = Archive(BASE_DIR / "zip.cbz")
+    builder = mock_builder_in_source.return_value
+    builder.get_object.side_effect = [mock_img, mock_label, mock_box]
+    thumb.create_thumb(PageInfo(path=Path("zip.cbz"), page_idx=0))
+    return thumb
+
+
+def test_idle(
+    thumb_create_thumb_called, mock_glib_idle_source, mocker, mock_cover_cache,
+):
+    thumb_create_thumb_called  # silence unused
+    mock_glib_idle_source  # silence unused
+    idle = mock_glib_idle_source.return_value
+    idle.set_callback.assert_called_once()
+    callback = idle.set_callback.call_args_list[0][0][0]
+
+    mock_cover_cache.cover.return_value = image.new('RGB', (64, 64))
+    rv = callback(mocker.ANY)
+    mock_cover_cache.cover.assert_called_once_with(
+        BASE_DIR, Path("zip.cbz"), 0
+    )
+    assert rv == GLib.SOURCE_CONTINUE
+
+    rv = callback(mocker.ANY)
+    assert rv == GLib.SOURCE_REMOVE
+
+
+def test_archive_set_twice(thumb_create_thumb_called, mock_glib_idle_source):
+    thumb = thumb_create_thumb_called
+    thumb.archive = Archive(BASE_DIR / "rar.cbr")
+    mock_glib_idle_source.return_value.destroy.assert_called_once()
+
+
+def test_ensure_idle_twice(
+    thumb_create_thumb_called, mock_glib_idle_source, mock_box, mock_label,
+    mock_img, mock_builder_in_source,
+):
+    thumb = thumb_create_thumb_called
+    builder = mock_builder_in_source.return_value
+    builder.get_object.side_effect = [mock_img, mock_label, mock_box]
+    thumb.create_thumb(PageInfo(Path("rar.cbr"), page_idx=1))
+    mock_glib_idle_source.assert_called_once()
+
+
+def test_page_activated(thumb, mock_flowbox, mocker, mock_view):
+    thumb  # silence unused
+    callback = mock_flowbox.connect.call_args_list[0][0][1]
+    child = mocker.MagicMock(spec=Gtk.Bin)
+    child.get_child.return_value.page_idx = 2
+    callback(mocker.ANY, child)
+    assert mock_view.page_idx == 2
+
+
+def test_scroll_to(thumb, mock_flowbox, mock_scrolled_window):
+    thumb.scroll_to(5)
+    child = mock_flowbox.get_child_at_index.return_value
+    mock_flowbox.select_child.assert_called_once_with(child)
+    mock_scrolled_window.get_vadjustment.return_value \
+        .set_value.assert_called_once_with(child.get_allocation.return_value.y)
+
+
+def test_scroll_to_no_such_page(thumb, mock_flowbox):
+    mock_flowbox.get_child_at_index.return_value = None
+    thumb.scroll_to(5)
+    mock_flowbox.select_child.assert_not_called()
